9b3fa7060e53b09c5c4f2742025ce42857241586
Jihad Hasan <mdjihat0021@gmail.com>
2024-03-29 03:34:00 +0600

    finished implementing Pagination in service section

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,13 +1,45 @@
+import { useEffect, useState } from "react";
 import useUserData from "../../Hook/useUserData";
 
 const Services = () => {
   const userData = useUserData();
-  // console.log(userData?.services);
-  const filteredServices = userData?.services?.filter(
-    (service) => service?.enabled === true
+  const [pageNumber, setPageNumber] = useState(1);
+  const [cardsPerPage, setCardsPerPage] = useState(3);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setCardsPerPage(1);
+      } else {
+        setCardsPerPage(3);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const numberOfPages = Math.ceil(
+    (userData?.services?.length || 0) / cardsPerPage
   );
 
-  console.log(filteredServices);
+  const updatePageNumber = (newPageNumber) => {
+    if (newPageNumber > 0 && newPageNumber <= numberOfPages) {
+      setPageNumber(newPageNumber);
+    }
+  };
+
+  const indexOfLastCard = pageNumber * cardsPerPage;
+  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+  const currentCards = userData?.services?.slice(
+    indexOfFirstCard,
+    indexOfLastCard
+  );
 
   return (
     <section className="services" id="services">
@@ -15,7 +47,7 @@ const Services = () => {
         <span>Services</span>
       </h1>
       <div className="box-container">
-        {filteredServices?.map((service) => (
+        {currentCards?.map((service) => (
           <div key={service?._id} className="box" data-aos="zoom-in">
             {/* Regular content */}
             <div className="regular-content">
@@ -27,10 +59,70 @@ const Services = () => {
             <div className="hover-content box flex flex-col px-10 items-center justify-center space-y-6">
               <h1 className="text-4xl font-extrabold">{service?.name}</h1>
               <p>{service?.desc}</p>
-              <p className="font-extrabold"> {service?.charge}</p>
+              <p className="font-extrabold">{service?.charge}</p>
             </div>
           </div>
         ))}
+      </div>
+      {/* Pagination */}
+      <div className="flex select-none justify-center items-center gap-3 mt-10">
+        {/* left arrow */}
+        <div
+          onClick={() => updatePageNumber(pageNumber - 1)}
+          className={`hover:scale-110 scale-100 transition-all duration-200 cursor-pointer hover:bg-white px-1 py-1 rounded-full ${
+            pageNumber === 1 ? "opacity-50 cursor-not-allowed" : ""
+          }`}
+        >
+          <svg
+            className="w-12"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M15 7L10 12L15 17"
+              stroke="#0284C7"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+        <div className="flex justify-center items-center gap-2 bg-white p-2 shadow-lg rounded-full">
+          {[...Array(numberOfPages).keys()].map((item) => (
+            <div
+              onClick={() => setPageNumber(item + 1)}
+              className={`cursor-pointer hover:scale-110 text-sm scale-100 transition-all duration-200 px-5 ${
+                pageNumber === item + 1 ? "bg-sky-500 text-white" : "bg-white"
+              } border-sky-300 font-semibold text-gray-700 py-5 rounded-full`}
+              key={item}
+            >
+              {item + 1}
+            </div>
+          ))}
+        </div>
+        {/* right arrow */}
+        <div
+          onClick={() => updatePageNumber(pageNumber + 1)}
+          className={`hover:scale-110 scale-100 transition-all duration-200 cursor-pointer hover:bg-white px-1 py-1 rounded-full ${
+            pageNumber === numberOfPages ? "opacity-50 cursor-not-allowed" : ""
+          }`}
+        >
+          <svg
+            className="w-12"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 7L15 12L10 17"
+              stroke="#0284C7"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
       </div>
     </section>
   );
